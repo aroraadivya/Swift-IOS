7a629f18bba7907816d26a290e210d703d8cc7ed
Divya Arora <111882666+divya0o1@users.noreply.github.com>
2024-10-03 11:38:30 +0530

    updates

--- Lab - Classes 2.playground/Pages/1. Exercise - Define a Base Class.xcplaygroundpage/Contents.swift
+++ Lab - Classes 2.playground/Pages/1. Exercise - Define a Base Class.xcplaygroundpage/Contents.swift
@@ -22,11 +22,9 @@ class Spaceship{
         }
     }
 }
-
 //:  Create a `let` constant called `falcon` and assign it to an instance of `Spaceship`. After initialization, set `name` to "Falcon."
  let falcon=Spaceship()
 falcon.name="Falcon"
-
 //:  Go back and add a method called `moveLeft()` to the definition of `Spaceship`. This method should adjust the position of the spaceship to the left by one. Add a similar method called `moveRight()` that moves the spaceship to the right. Once these methods exist, use them to move `falcon` to the left twice and to the right once. Print the new position of `falcon` after each change in position.
 print(falcon.position)
 falcon.moveLeft()
@@ -35,11 +33,9 @@ falcon.moveLeft()
 print(falcon.position)
 falcon.moveRight()
 print(falcon.position)
-
 //:  The last thing `Spaceship` needs for this example is a method to handle what happens if the ship gets hit. Go back and add a method `wasHit()` to `Spaceship` that will decrement the ship's health by 5, then if `health` is less than or equal to 0 will print "Sorry, your ship was hit one too many times. Do you want to play again?" Once this method exists, call it on `falcon` and print out the value of `health`.
 falcon.wasHit()
 print(falcon.health)
-
 /*:
 page 1 of 4  |  [Next: Exercise - Create a Subclass](@next)
  */
